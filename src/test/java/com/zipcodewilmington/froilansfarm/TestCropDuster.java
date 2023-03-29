33e35b10099c7929bcb2045d8d2206128849069d
Nina <nina.emiko.mcnair@gmail.com>
2023-03-29 18:07:53 -0400

    Added more Aircraft tests

--- src/test/java/com/zipcodewilmington/froilansfarm/TestCropDuster.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/TestCropDuster.java
@@ -1,6 +1,9 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import javax.accessibility.AccessibleTable;
 
 public class TestCropDuster {
     @Test
@@ -23,4 +26,33 @@ public class TestCropDuster {
         CropDuster cropDuster = new CropDuster();
         Assert.assertTrue(cropDuster instanceof Rideable);
     }
+    @Test
+    public void testFertilizeCrops() {
+        Field field = new Field();
+        Pilot pilot = new Pilot();
+        pilot.rideCropDuster();
+        int expected = 1;
+        int actual = field.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatFarmerCannotRideCropDuster() {
+        Field field = new Field();
+        Farmer farmer = new Farmer();
+        farmer.rideCropDuster();
+        int expected = 0;
+        int actual = field.size();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testThatBotanistCannotRideCropDuster() {
+        Field field = new Field();
+        Botanist botanist = new Botanist();
+        botanist.rideCropDuster();
+        int expected = 0;
+        int actual = field.size();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
