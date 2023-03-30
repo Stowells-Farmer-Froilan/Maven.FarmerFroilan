0a48d6194271f517d5aca8b90556b1364682a3f2
ule270 <u.le270@gmail.com>
2023-03-30 17:43:34 -0400

    eat tests written.

--- src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Chicken.java
@@ -2,31 +2,42 @@ package com.zipcodewilmington.froilansfarm;
 
 public class Chicken extends Produce{
     Chicken chicken;
-//    private int numberChickens = 15;
     private int eggsLaid;
+    private int cornEaten;
+    private int tomatoEaten;
     private Object unFertilizedEgg;
     private int fertilizedEggs;
 
     public Chicken(){
         eggsLaid = 0;
     }
-
     public String makeNoise() {
         String noise = "Cluck";
         return noise;
     }
-
     public void layEgg(){
         eggsLaid++;
     }
-
     public int getEggsLaid(){
         return eggsLaid;
     }
-
     public boolean hasBeenFertilized(Object unFertilizedEgg){
         boolean fertilized = false;
         return fertilized;
+    }
+
+    public int eatCorn(){
+        if (cornStorage.size() > 3){
+            cornEaten = 3;
+        }
+        return cornEaten;
+    }
+
+    public int eatTomato(){
+        if (tomatoStorage.size() > 1){
+            tomatoEaten = 1;
+        }
+        return tomatoEaten;
     }
 
 }
