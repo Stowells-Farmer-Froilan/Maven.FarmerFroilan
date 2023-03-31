c14e6e17e2e141e0f4d573059f0c7cbe009c0041
Nina <nina.emiko.mcnair@gmail.com>
2023-03-30 21:23:38 -0400

    Minor changes to operates methods

--- src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
@@ -1,15 +1,17 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class CropDuster extends Aircraft {
+    Field field;
+    CropRow<Crop> cropRow;
     public String makeNoise(){
         return "I'm a crop duster yo";
     }
     public void operates(){
 
         for (int i = 0; i < field.size(); i++){
-            if (cropRow.type() == CornStalk) {
+            if (cropRow.get(i) == CornStalk) {
                 cropRow.hasBeenFertilized = true;
-            } else if (cropRow.type() == TomatoPlant) {
+            } else if (cropRow.get(i) == TomatoPlant) {
                 cropRow.hasBeenFertilized = true;
             }
         }
--- src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
@@ -1,20 +1,21 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class Tractor extends FarmVehicle{
-
+    Field field;
+    CropRow<Crop> cropRow;
     public String makeNoise() {
         return "I'm a tractor yo";
     }
 
     public void operates() {
         for (int i = 0; i < field.size(); i++) {
-            if (cropRow.type() == CornStalk) {
-                if (cropRow.hasBeenFertilized == true) {
-                    cropRow.hasBeenHarvested = true;
+            if (cropRow.get(i) == CornStalk) {
+                if (cropRow.get(i).hasBeenFertilized == true) {
+                    cropRow.get(i).hasBeenHarvested = true;
                 }
-            } else if (cropRow.type == TomatoPlant) {
-                if (cropRow.hasBeenFertilized == true) {
-                    cropRow.hasBeenHarvested = true;
+            } else if (cropRow.get(i) == TomatoPlant) {
+                if (cropRow.get(i).hasBeenFertilized == true) {
+                    cropRow.get(i).hasBeenHarvested = true;
                 }
             }
         }
