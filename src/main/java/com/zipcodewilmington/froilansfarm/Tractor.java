09a09b5e93d4cfec7f803be6df3f31abdd7529a5
Nina <nina.emiko.mcnair@gmail.com>
2023-03-30 20:57:15 -0400

    Completed base of operates methods

--- src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Tractor extends FarmVehicle {
+public class Tractor extends FarmVehicle{
     public String makeNoise(){
         return "I'm a tractor yo";
+    }
+    public void operates(){
+
+        for (int i = 0; i < field.size(); i++){
+            if (cropRow.type() == CornStalk){
+                if (cropRow.hasBeenFertilized == true) {
+                    cropRow.hasBeenHarvested = true;
+                }
+            } else if (cropRow.type == TomatoPlant){
+                if (cropRow.hasBeenFertilized == true) {
+                    cropRow.hasBeenHarvested = true;
+                }
+            }
+        }
     }
 }
