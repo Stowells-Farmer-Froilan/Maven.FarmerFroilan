3e76cae6fbeda0bddb6862024d1493a0713aab6d
Nina <nina.emiko.mcnair@gmail.com>
2023-03-30 20:21:05 -0400

    All vehicle make noise tests passing

--- src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/CropDuster.java
@@ -1,19 +1,7 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class CropDuster extends Aircraft {
-    public static void fertilize(){
-
-        for (Crop c : CropRow){
-            if (c instanceof CornStalk) {
-                cornStorage += 10;
-            }else if (c instanceof TomatoPlant) {
-                tomatoStorage += 10;
-            }
-        }
-
-        for (egg e : something) {
-            eggStorage += 1;
-        }
-
+    public String makeNoise(){
+        return "I'm a crop duster yo";
     }
 }
