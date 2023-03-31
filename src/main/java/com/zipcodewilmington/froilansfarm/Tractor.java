84b2776a342b8248ed2e75aa482b4e8b1c5e5898
Nina <nina.emiko.mcnair@gmail.com>
2023-03-30 21:04:43 -0400

    Minor changes to vehcile classes

--- src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Tractor.java
@@ -1,17 +1,18 @@
 package com.zipcodewilmington.froilansfarm;
 
 public class Tractor extends FarmVehicle{
-    public String makeNoise(){
+
+    public String makeNoise() {
         return "I'm a tractor yo";
     }
-    public void operates(){
 
-        for (int i = 0; i < field.size(); i++){
-            if (cropRow.type() == CornStalk){
+    public void operates() {
+        for (int i = 0; i < field.size(); i++) {
+            if (cropRow.type() == CornStalk) {
                 if (cropRow.hasBeenFertilized == true) {
                     cropRow.hasBeenHarvested = true;
                 }
-            } else if (cropRow.type == TomatoPlant){
+            } else if (cropRow.type == TomatoPlant) {
                 if (cropRow.hasBeenFertilized == true) {
                     cropRow.hasBeenHarvested = true;
                 }
